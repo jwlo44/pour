6c38e6537ef79af42cd746d1c549f50e9a027b08
Julia Wlo <Julia Wlo>
2017-01-11 18:51:09 -0500

    water toggles when you press e

---
+++ Assets/WaterOnSpace.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class WaterOnSpace : MonoBehaviour {
+
+    public GameObject water;
+
+	// Use this for initialization
+	void Start () {
+        water.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (CrossPlatformInputManager.GetButtonDown("Action"))
+        {
+            water.SetActive(true);
+        }
+        if (CrossPlatformInputManager.GetButtonUp("Action"))
+        {
+            water.SetActive(false);
+        }
+	}
+}
