a884cb06096bae4a5911ad2bb71f314b22bdb77d
Julia Wlo <Julia Wlo>
2017-01-11 16:12:31 -0500

    first draft flower grow & wiggle

---
+++ Assets/GrowWhenWatered.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrowWhenWatered : MonoBehaviour {
+
+    bool grow = false;
+    public Vector3 growthRate = new Vector3(0, .1f, 0);
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (grow)
+        {
+            gameObject.transform.localScale += growthRate;
+        }
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "wateringCan")
+        {
+            grow = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "wateringCan")
+        {
+            grow = false;
+        }
+    }
+}
---
+++ Assets/wiggleDance.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wiggleDance : MonoBehaviour {
+    [Tooltip("the max amount this object will rotate by")]
+    public float wiggleAmount = 30f;
+    [Tooltip("the speed at which we wiggle")]
+    public float wiggleRate = 3;
+    public Vector3 rotateAround = new Vector3(0,0,1);
+
+    float amount = 0;
+    float direction = 1;
+
+    void Start()
+    {
+        gameObject.transform.Rotate(rotateAround, -wiggleAmount);
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (amount > wiggleAmount * 2)
+        {
+            amount = amount % wiggleAmount;
+            direction = -direction;
+        }
+        amount += wiggleRate;
+
+        this.gameObject.transform.Rotate(rotateAround, wiggleRate * direction);
+	}
+}
