542a8aa5af38e57fd19d835871f9c6b855de901a
Julia Wlo <Julia Wlo>
2017-01-25 15:19:39 -0500

    enable win text on win

--- Assets/scripts/SceneManager.cs
+++ Assets/scripts/SceneManager.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// enables a gameobject when winning
 public class SceneManager : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject enableOnWin;
+
+    private void OnWin()
+    {
+        enableOnWin.SetActive(true);
+    }
 
 	// Use this for initialization
 	void Start () {
-		
+        FlowerManager.OnWin += OnWin;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	void OnDestroy()
+    {
+        FlowerManager.OnWin -= OnWin;
+    }
 }
