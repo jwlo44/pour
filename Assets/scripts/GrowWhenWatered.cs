19a416a1b3d37156cc20dc7accfeab75da5c3a01
Julia Wlo <Julia Wlo>
2017-01-25 14:25:45 -0500

    max growth on grow script

--- Assets/scripts/GrowWhenWatered.cs
+++ Assets/scripts/GrowWhenWatered.cs
@@ -6,6 +6,7 @@ public class GrowWhenWatered : MonoBehaviour {
 
     bool grow = false;
     public Vector3 growthRate = new Vector3(0, .1f, 0);
+    public Vector3 maxGrowthScale = new Vector3(1, 1, 1);
 	// Use this for initialization
 	void Start () {
 		
@@ -13,9 +14,21 @@ public class GrowWhenWatered : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        // grows the localscale of the transform while colliding with the watering can until reaching max growth
 		if (grow)
         {
-            gameObject.transform.localScale += growthRate;
+            if (gameObject.transform.localScale.x < maxGrowthScale.x)
+            {
+                gameObject.transform.localScale += new Vector3(growthRate.x, 0);
+            }
+            if (gameObject.transform.localScale.y < maxGrowthScale.y)
+            {
+                gameObject.transform.localScale += new Vector3(0, growthRate.y);
+            }
+            if (gameObject.transform.localScale.z < maxGrowthScale.z)
+            {
+                gameObject.transform.localScale += new Vector3(0,0,growthRate.z);
+            }
         }
 	}
 
